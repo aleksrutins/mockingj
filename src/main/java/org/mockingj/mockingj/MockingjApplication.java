c17ec9c9f0032966c02ad151b3124c9c308da36e
Aleks Rutins <aleks@rutins.com>
2023-02-05 17:28:13 -0500

    first commit

---
+++ src/main/java/org/mockingj/mockingj/MockingjApplication.java
@@ -0,0 +1,13 @@
+package org.mockingj.mockingj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockingjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockingjApplication.class, args);
+	}
+
+}
