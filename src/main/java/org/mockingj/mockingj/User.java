92ab64a076246efb11673cbde9955d166a4a8184
Aleks Rutins <aleks@rutins.com>
2023-02-06 10:50:23 -0500

    Add user model

---
+++ src/main/java/org/mockingj/mockingj/User.java
@@ -0,0 +1,41 @@
+package org.mockingj.mockingj;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String username;
+    private String email;
+    private byte[] password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public byte[] getPassword() {
+        return password;
+    }
+
+    public void setPassword(byte[] password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/org/mockingj/mockingj/UserRepository.java
@@ -0,0 +1,7 @@
+package org.mockingj.mockingj;
+
+import org.springframework.data.repository.Repository;
+
+public interface UserRepository extends Repository<User, Long> {
+    
+}
